44180320bd2f6acac1c3eacb46962687b3845fad
LeftyJekyll <alhinckley777@gmail.com>
2015-11-30 16:32:10 -0600

    Added Scheduler functionality

--- hw6/scheduler.cpp
+++ hw6/scheduler.cpp
@@ -11,7 +11,7 @@
 	1. type:	'make' while inside the correct directory
 	2. type:	'./scheduler [n]'  to EXECUTE with integer n child processes
 	OR
-	3. type:    './scheudler'  for a HELP message
+	3. type:    './scheduler'  for a HELP message
 	DISTRIBUTION/SUBTASKS:
 			Aaron Hinckley: 50%
 
@@ -23,12 +23,15 @@
 #include <unistd.h>
 #include <cstdlib>
 #include "info.h"
+#include <vector>
 
 using namespace std;
 
 int pipeToChild[2];
 int bytes;
+int timer = 0;
 const int rHead = 0, wHead = 1;
+vector<Info> readyQ;
 
 Info recieved, sent;
 
@@ -48,6 +51,37 @@ Info readPipe() {
 		exit(EXIT_FAILURE);
 	}
 	return temp;
+}
+void displayInfo(vector<Info> temp){
+	cout << "------------------------------------------------------" << endl;
+	for(int i = 0; i < temp.size(); i ++){
+		cout << "Process number: " << i << endl;
+		cout << "	CPU Burst Time: " << temp.at(i).burst << endl;
+		cout << "	Arrival Time: " << temp.at(i).arrivalTime << endl;
+		cout << "	Total Time: " << temp.at(i).totalTime << endl;
+	}
+    cout << "------------------------------------------------------" << endl;
+
+}
+void sortQ(vector<Info> readyQ){
+	vector<Info> temp = readyQ;
+	int counter = 0;
+	// Sorted Iteratively
+	while(counter != readyQ.size()){
+		int index;
+		for(int i = 0; i < temp.size(); i ++){
+			if(temp[i].burst < temp[index].burst){  // SJF Sorting
+				index = i;
+			}
+			else if(temp[i].burst == temp[index].burst){  // FCFS Sorting
+				if(temp[i].arrivalTime < temp[index].arrivalTime){
+					index = i;
+				}
+			}
+		}
+		readyQ[counter] = temp[index];
+		counter ++;
+	}
 }
 
 int main(int argc, char* argv[])
@@ -72,8 +106,13 @@ int main(int argc, char* argv[])
 
 	pid_t returnVal = fork();
 	if (returnVal > 0) {
-		// parent process
-
+		// Scheduler Code Here
+		for(int i = 0; i < n; i ++){
+			readyQ.push_back(readPipe());
+			timer += readyQ.at(i).arrivalTime;
+		}
+		displayInfo(readyQ);
+		
 	}
 	else if (returnVal < 0) {
 		perror("fork");
